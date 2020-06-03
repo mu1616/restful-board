4e6489d7237a41540b330b5f996b4a39ce62cd16
mu1616 <mu1616@naver.com>
2020-06-04 00:40:27 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulboard/RestfulBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulBoardApplication.class, args);
+    }
+
+}
