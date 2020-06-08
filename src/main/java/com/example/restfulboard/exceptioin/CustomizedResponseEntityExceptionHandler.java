c1c7c6117fde0d82fcdfd353a68ee46bf53d1681
mu1616 <mu1616@naver.com>
2020-06-09 02:36:58 +0900

    User 객체 유효성 검증코드 추가
    @Valid 추가
    handleMethodArgumentNotValid 메소드 오버라이드

--- src/main/java/com/example/restfulboard/exceptioin/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restfulboard/exceptioin/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.example.restfulboard.exceptioin;
 
 import com.example.restfulboard.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,18 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    // valid 유효성 검사 실패 시 예외처리
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+                "Validation failed", ex.getMessage());
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
