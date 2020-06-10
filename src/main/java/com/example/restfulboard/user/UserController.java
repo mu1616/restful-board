c5c5dd76912a8106dcf73c8c4e3528f59a7f00a6
mu1616 <mu1616@naver.com>
2020-06-10 22:15:30 +0900

    HATEOAS 적용
    /users/{id} 에 HATEOAS 적용

--- src/main/java/com/example/restfulboard/user/UserController.java
+++ src/main/java/com/example/restfulboard/user/UserController.java
@@ -1,12 +1,24 @@
 package com.example.restfulboard.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -22,13 +34,34 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public ResponseEntity<MappingJacksonValue> retrieveUser(@PathVariable int id, HttpServletRequest request){
         User user = service.findOne(id);
 
         if(user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
-        return service.findOne(id);
+
+        // HATEOAS
+        //retrieveAllUsers 메소드(/users)를 "all-user" 와 연결
+        Resource<User> resource = new Resource<>(user);
+        /*
+        ControllerLinkBuilder linkTo = ControllerLinkBuilder.linkTo(
+                ControllerLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        */
+        resource.add(new Link(request.getRequestURL().toString()).withSelfRel());
+        resource.add(linkTo(methodOn(UserController.class).retrieveAllUsers())
+                .withRel("all-users")
+                .withType("GET"));
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                //아래의 필드들을 가져온다. JsonIgnore 되어있는 경우 가져오지 않음.
+                .filterOutAllExcept("id", "name", "joinDate");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(resource);
+        mapping.setFilters(filters);
+
+        return ResponseEntity.ok(mapping);
     }
 
     @PostMapping("/users")
