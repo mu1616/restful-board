fd52cedca01bb6718461edec120c1a5a6266d9d9
mu1616 <mu1616@naver.com>
2020-06-14 19:06:27 +0900

    JPA 설정
    jpa dependency 추가
    entity 설정
    초기 데이터 설정(data.sql)

--- src/main/java/com/example/restfulboard/user/User.java
+++ src/main/java/com/example/restfulboard/user/User.java
@@ -10,6 +10,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -19,18 +23,25 @@ import java.util.Date;
 @NoArgsConstructor
 @JsonFilter("UserInfo")  //UserInfo라는 이름의 JsonFilter 생성
 @ApiModel(description = "사용자 상제 정보를 위한 도메인 객체")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     // 두글자 이상, 에러 메시지
     @Size(min=2, message ="Name은 2글자 이상 입력해 주세요.")
     @ApiModelProperty(notes = "사용자 이름을 입력해주세요")
     private String name;
+
     @Past
     @ApiModelProperty(notes = "사용자의 등록일을 입력해주세요")
     private Date joinDate;
+
     //@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)  //@JsonIgnore는 읽기 쓰기 둘다 안되는듯?
     @ApiModelProperty(notes = "사용자 패스워드를 입력해주세요")
     private String password;
+
     //@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ApiModelProperty(notes = "사용자의 주민등록번호를 입력해주세요")
     private String ssn;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user (join_date, name, password, ssn) values (sysdate(), 'user1', 'test', '970816-1111111')
+insert into user (join_date, name, password, ssn) values (sysdate(), 'user2', 'test', '960816-1111111')
+insert into user (join_date, name, password, ssn) values (sysdate(), 'user3', 'test', '950816-1111111')
