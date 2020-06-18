d11cb5c09862c2aa72d7870e4d5e4e37e69d199d
mu1616 <mu1616@naver.com>
2020-06-19 00:11:05 +0900

    JPA를 이용한 사용자 추가, 삭제 구현
    DELETE /jpa/users/{id}
    POST /jpa/users

--- src/main/java/com/example/restfulboard/user/UserJpaController.java
+++ src/main/java/com/example/restfulboard/user/UserJpaController.java
@@ -3,10 +3,12 @@ package com.example.restfulboard.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,5 +39,22 @@ public class UserJpaController {
         resource.add(linkTo.withRel("all-users"));
 
         return resource;
+    }
+
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/jpa/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+       User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
